445be94430273f6ec9c26283f22325e54348959c
Andrey Unguryanu <skorpionez@list.ru>
2023-11-01 20:05:10 +0400

    commit add Task05.java

--- src/Task05.java
+++ src/Task05.java
@@ -102,6 +102,7 @@ public class Task05 {
         }
         System.out.println("Знак: " + zodiacSign);
         System.out.println("Год: " + zodiacGod);
-        }
-        }
+        System.out.println();
+    }
+}
 
